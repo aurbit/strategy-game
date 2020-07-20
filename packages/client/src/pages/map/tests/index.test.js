561fde2146ed7c09e392ea78ca6efc5db9b6fdf4
Renate Gouveia <renate.gouveia@gmail.com>
2020-07-21 02:59:22 +0800

    Change let to const as per lint rules

--- packages/client/src/pages/map/tests/index.test.js
+++ packages/client/src/pages/map/tests/index.test.js
@@ -9,13 +9,13 @@ test('render the map', () => {
   }
 
   const isLand = index => {
-    let loc = (index / 8).toFixed(0)
-    let bitpos = 7 - (index % 8)
+    const loc = (index / 8).toFixed(0)
+    const bitpos = 7 - (index % 8)
     return pullbit(map[loc], bitpos)
   }
 
   const main = () => {
-    let newl = 112
+    const newl = 112
     let out = ''
     for (let i in [...Array(112 * 64).keys()]) {
       out = out + isLand(i)
