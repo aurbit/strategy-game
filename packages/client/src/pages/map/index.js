45dc99e96dd7bf185508313314fa49a2f8ed1662
Renate Gouveia <renate.gouveia@gmail.com>
2020-07-21 02:49:22 +0800

    Add standard to help with lint and formatting

--- packages/client/src/pages/map/index.js
+++ packages/client/src/pages/map/index.js
@@ -1,22 +1,22 @@
-import React from "react";
-import styles from "./index.module.css";
-import logo from "./logo.svg";
-import "./App.css";
-import Hello from "shared/components/Hello";
+import React from 'react'
+import styles from './index.module.css'
+import logo from './logo.svg'
+import './App.css'
+import Hello from 'shared/components/Hello'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
@@ -24,7 +24,7 @@ function App() {
         <div className={styles.helloWorld}>CSS MODULE TEST</div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- packages/client/src/shared/components/Hello/index.js
+++ packages/client/src/shared/components/Hello/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react'
 
 const HelloTest = () => {
-  return <div>Hello</div>;
-};
-export default HelloTest;
+  return <div>Hello</div>
+}
+export default HelloTest
