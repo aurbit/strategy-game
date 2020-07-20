c3e932c1d72ee2d5007c7bebb872039411d7156e
Renate Gouveia <renate.gouveia@gmail.com>
2020-07-21 03:12:28 +0800

    Fix lint issue

--- packages/client/src/pages/map/tests/index.test.js
+++ packages/client/src/pages/map/tests/index.test.js
@@ -9,7 +9,7 @@ test('render the map', () => {
   }
 
   const isLand = index => {
-    let loc = (index / 8).toFixed(0)
+    const loc = (index / 8).toFixed(0)
     const bitpos = 7 - (index % 8)
     return pullbit(map[loc], bitpos)
   }
@@ -17,7 +17,7 @@ test('render the map', () => {
   const main = () => {
     const newl = 112
     let out = ''
-    for (let i in [...Array(112 * 64).keys()]) {
+    for (const i in [...Array(112 * 64).keys()]) {
       out = out + isLand(i)
       if (i % newl === 0) {
         console.log(out)
