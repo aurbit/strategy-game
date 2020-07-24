585b2fcabb7d9705f2d572b5ede119aee8d12572
Chad Lynch <chyad@hotmail.com>
2020-07-24 22:13:07 +0800

    removed some packages

---
+++ packages/client/src/shared/context/logger.js
@@ -0,0 +1,13 @@
+const logger = reducer => {
+  const reducerWithLogger = (state, action) => {
+    const next = reducer(state, action)
+    console.log('%cPrevious State:', 'color: #9E9E9E; font-weight: 700;', state)
+    console.log('%cAction:', 'color: #00A7F7; font-weight: 700;', action)
+    console.log('%cNext State:', 'color: #47B04B; font-weight: 700;', next)
+    return next
+  }
+
+  return reducerWithLogger
+}
+
+export default logger
